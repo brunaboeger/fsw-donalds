42749b34f0288d508374c037eb9ab8b3e353ab2e
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-02-21 18:53:49 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,63 @@
+// import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  // const restaurant = await getRestaurantBySlug(slug);
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center px-6 pt-24">
+      {/* LOGO E TÍTULO */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+
+      {/* BOAS VINDAS */}
+      <div className="space-y-2 pt-16 text-center">
+        <h3 className="text-2xl font-semibold">Boas vindas!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+
+      {/* CONSUMAÇÃO */}
+      <div className="grid grid-cols-2 pt-14 gap-4">
+        <ConsumptionMethodOption imageUrl="/dine_in.png" imageAlt="Para comer aqui" buttonText="Para comer aqui" />
+        <ConsumptionMethodOption imageUrl="/takeaway.png" imageAlt="Para levar" buttonText="Para levar" />
+      </div>
+
+      <div className="mt-14">
+        <Button asChild>
+          <Link href="/">
+            Voltar
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-const HomePage = () => {
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { db } from "@/lib/prisma";
+
+const HomePage = async () => {
+  const restaurants = await db.restaurant.findMany();
+
   return (
-    <div className="border border-gray-300 p-5">
-      <h1 className="mb-5 text-slate-500">Hello World</h1>
-      <Button>Um botão</Button>
+    <div className="p-14 h-screen">
+      <div className="mb-10">
+        <h1 className="mb-2 text-3xl font-medium">Restaurantes</h1>
+        <p className="text-gray-500">Encontrados: {restaurants.length}</p>
+      </div>
+      <div>
+        {restaurants.map((restaurant) => (
+          <Card key={restaurant.id} className="mb-4 flex items-center justify-between hover:bg-gray-100 transition-all">
+            <CardContent className="flex pt-6">
+              <CardHeader className="p-0 mr-6 relative w-[50px] h-[50px]">
+                <Image src={restaurant.avatarImageUrl} alt={restaurant.name} className="object-contain" fill />
+              </CardHeader>
+              <CardContent className="flex flex-col p-0">
+                <CardTitle className="text-xl">{restaurant.name}</CardTitle>
+                <CardDescription>{restaurant.description}</CardDescription>
+              </CardContent>
+            </CardContent>
+            <CardFooter className="pt-6">
+              <Button asChild>
+                <Link href={restaurant.slug}>
+                  Acessar
+                </Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
